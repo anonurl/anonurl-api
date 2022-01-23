ad3b193ed1dc5a0d12da72c631db9afab0563f0c
z3ox1s <z3ox1s@protonmail.com>
2022-01-23 16:32:50 -0300

    Release v1.0.0b

--- database/database.go
+++ database/database.go
@@ -19,8 +19,10 @@ type Report struct {
     Message string `bson:"message"`
 }
 
-var clientOptions = options.Client().ApplyURI(os.Getenv("MONGO_URI"))
-var client, _ = mongo.Connect(context.TODO(), clientOptions)
-var ctx, _ = context.WithTimeout(context.Background(), 10 * time.Second)
-var urls = client.Database("anonurl").Collection("urls")
-var reports = client.Database("anonurl").Collection("reports")
+var (
+    clientOptions = options.Client().ApplyURI(os.Getenv("MONGO_URI"))
+    client, _ = mongo.Connect(context.TODO(), clientOptions)
+    ctx, _ = context.WithTimeout(context.Background(), 10 * time.Second)
+    urls = client.Database("anonurl").Collection("urls")
+    reports = client.Database("anonurl").Collection("reports")
+)
